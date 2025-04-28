92b2c7456e7f55c4fe609d77b79010e711cfb9d9
nihal514t <nihal514t@gmail.com>
2025-04-28 11:17:28 +0530

    removed bycript and installed bycriptjs on server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const serverless = require('serverless-http');
 
